a54c5e50b46425143ab4328df05959e62c9ac07d
David Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-02-28 18:54:14 -0500

    updated ex7

--- ex7.py
+++ ex7.py
@@ -1,11 +1,15 @@
-def fonct1(x, y, z=0):
+# Créer une fonction qui permet de calculer la fonction suivante: (x + y) / z.
+# Essayer ensuite avec z = 0.
+
+def fonct1(x, y, z):             # la fonction 1 est ici complètement inutile
     
     def fonct2(x, y, z):
-        return ((x+y)/z)
+        return (x+y)/z
     
     return fonct2(x, y, z)
 
+
 a = 1
 b = 2
-c = 3
+c = 3                            # si c = 0 le message d'erreur ZeroDivisionError va apparaître
 print(fonct1(a,b,c))
