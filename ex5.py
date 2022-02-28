d14f20b214c2ed2fee324ba154236841f46600d2
David Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-02-28 18:38:04 -0500

    ex5 updated

--- ex5.py
+++ ex5.py
@@ -1,10 +1,14 @@
+# Créer une fonction prenant 4 paramètres et retournant la somme des deux premiers multipliés par 
+# le 3ième et divisé par le 4ème.
+
 def fonction(a,b,c,d):
-    return (((a+b)*c)/d)
+    som = a+b
+    return (som*c)/d
+
 
 w = 1
 x = 2
 y = 3
 z = 4
-
 print(fonction(w,x,y,z))
 
