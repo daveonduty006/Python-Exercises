20266c8d634274bdec670b97df106ac4069a82b8
David Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-02-28 18:46:12 -0500

    ex6 updated

--- ex6.py
+++ ex6.py
@@ -1,8 +1,16 @@
+# Créer une fonction qui calcule la moyenne de 4 paramètre et qui retourne aussi la somme de ces 4 
+# paramètres.
+
 def fonction(a,b,c,d):
-    return ((a+b+c+d)/4), (a+b+c+d)
+    som = a+b+c+d
+    return som/4, som
+
 
 w = 2
 x = 3
 y = 4
 z = 5
-print(fonction(w,x,y,z))
+m, n = fonction(w,x,y,z)
+print(m)
+print(n)
+
