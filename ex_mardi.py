55e68a93dd2ab3559ef67274428245d144d7a51a
daveonduty006 <95148064+daveonduty006@users.noreply.github.com>
2022-03-01 12:02:35 -0500

    add ex_mardi

---
+++ ex_mardi.py
@@ -0,0 +1,43 @@
+# Vous travaillez sur un système de géolocation s'intitulant à la recherche du pôle Nord utilisant 
+# des coordonnées sous la forme degrés minutes secondes (DMS).
+
+# Pour faciliter l'utilisation du système, on vous demande de créer un court programme permettant de 
+# convertir ces données sous la forme de degrés décimaux (DD).
+
+# De plus, considérant que le but de l'application est d'indiquer la distance des usagers du pôle 
+# Nord magnétique en plus d'indiquer leur position, on vous demande d'ajouter à votre programme une 
+# fonction permettant de calculer cette distance.
+
+# Versionner votre travail avec GitHub desktop et publié le sur votre profil GitHub Web une fois 
+# terminé.
+
+import math
+
+def dms_to_DD(deg_dms, min_dms, sec_dms):
+    deg_dd = float(deg_dms)
+    min_dd = min_dms/60
+    sec_dd = sec_dms/3600
+    return deg_dd+min_dd+sec_dd
+
+def distance_du_nord(coord_lat, coord_long):
+    distance_lat = (86-coord_lat)**2
+    distance_long = (172-coord_long)**2
+    return math.sqrt(distance_lat+distance_long)*111.16
+
+
+DEG_LAT= 35
+MIN_LAT= 39
+SEC_LAT= 10.08
+
+DEG_LONG= 139
+MIN_LONG= 50
+SEC_LONG= 21.84
+
+mes_coord_lat = dms_to_DD(DEG_LAT, MIN_LAT, SEC_LAT)
+mes_coord_long = dms_to_DD(DEG_LONG, MIN_LONG, SEC_LONG)
+
+print(distance_du_nord(mes_coord_lat, mes_coord_long))
+
+
+# le nom des constants en MAJUSCULES (corrigé)
+# pas de A,B,C en noms de paramètre dans les fonctions
