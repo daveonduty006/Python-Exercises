439dd8a601e4d428c94ee35be10786bc2d45a8ad
daveonduty006 <95148064+daveonduty006@users.noreply.github.com>
2022-03-04 12:45:46 -0500

    add comments from peer

--- ex_mardi.py
+++ ex_mardi.py
@@ -18,26 +18,24 @@ def dms_to_DD(deg_dms, min_dms, sec_dms):
     min_dd = min_dms/60
     sec_dd = sec_dms/3600
     return deg_dd+min_dd+sec_dd
-
+# distance de quel nord? # deux lignes vides entre définition de fonction
 def distance_du_nord(coord_lat, coord_long):
     distance_lat = (86-coord_lat)**2
     distance_long = (172-coord_long)**2
     return math.sqrt(distance_lat+distance_long)*111.16
 
+# définir le degré-minute-seconde de quoi (dans ce cas, degré-minute-seconde DMS)
+# définir quelle ville est représentée par ces données
+DEG_LAT = 35
+MIN_LAT = 39
+SEC_LAT = 10.08
 
-DEG_LAT= 35
-MIN_LAT= 39
-SEC_LAT= 10.08
-
-DEG_LONG= 139
-MIN_LONG= 50
-SEC_LONG= 21.84
+DEG_LONG = 139
+MIN_LONG = 50
+SEC_LONG = 21.84
 
 mes_coord_lat = dms_to_DD(DEG_LAT, MIN_LAT, SEC_LAT)
 mes_coord_long = dms_to_DD(DEG_LONG, MIN_LONG, SEC_LONG)
 
 print(distance_du_nord(mes_coord_lat, mes_coord_long))
 
-
-# le nom des constants en MAJUSCULES (corrigé)
-# pas de A,B,C en noms de paramètre dans les fonctions
