2720a777fef44b1a6bd6b2503edc406e22c95c2e
daveonduty006 <95148064+daveonduty006@users.noreply.github.com>
2022-03-11 15:06:59 -0500

    added

---
+++ ex9_2022-03-07.py
@@ -0,0 +1,16 @@
+#Exercice 9 (En équipe):
+#Partie 1:
+
+#Créer une calculatrice permettant de calculer combien couteraient en 2021, un article acheté en 1970. Utilisez le IPC moyenne annuelle de ces 
+#années respectives. Les données sont disponibles ici: https://www150.statcan.gc.ca/n1/pub/71-607-x/2018016/cpilg-ipcgl-fra.htm
+#Vous pouvez comparer vos résultats ici: https://www.banqueducanada.ca/taux/renseignements-complementaires/feuille-de-calcul-de-linflation/
+
+#Partie 2:
+#En utilisant le salaire minimum de ces années respectives, affichez le résultat en nombre d'heures de travail. Vous pouvez utiliser le salaire 
+#minimum fédéral ou provincial, tant que vous restez constant.
+
+#Partie 3:
+#Ensuite, comparez le nombre d'heures de travail nécessaire pour payer l'article au salaire minimum en 1970, en 2021 en fonction du coût de 
+#1970 ajusté pour l'inflation et en fonction de son coût en 2021. Faites attention d'utiliser le bon salaire minimum dans vos calculs, pour le 
+#nombre d'heures en 1970, utilisez le salaire de 1970, alors que pour le nombre d'heures en 2016, utilisez le salaire minimum de 2021.
+
