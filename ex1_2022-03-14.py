80ad632bde56a3178352bdcc5e8264746c8d86ba
David Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-03-14 15:58:39 -0400

    updated

--- ex1_2022-03-14.py
+++ ex1_2022-03-14.py
@@ -4,89 +4,89 @@
 
 def ordre_croissant(p1, p2, p3, p4):
     ordre = ""
-    if p1 <= p2 and p3 and p4:
-        ordre = str(p1) 
-        if p2 <= p3 and p4:
-            ordre = str(p1 + "," + p2) 
+    if p1 <= p2 and p1 <= p3 and p1 <= p4:
+        ordre = p1
+        if p2 <= p3 and p2 <= p4:
+            ordre = p1 + "," + p2
             if p3 <= p4:
-                ordre = str(p1 + "," + p2 + "," + p3 + "," + p4)
+                ordre = p1 + "," + p2 + "," + p3 + "," + p4
             else:
-                ordre = str(p1 + "," + p2 + "," + p4 + "," + p3) 
-        elif p3 <= p2 and p4:
-            ordre = str(p1 + "," + p3)
+                ordre = p1 + "," + p2 + "," + p4 + "," + p3
+        elif p3 <= p2 and p3 <= p4:
+            ordre = p1 + "," + p3
             if p2 <= p4:
-                ordre = str(p1 + "," + p3 + "," + p2 + "," + p4)
+                ordre = p1 + "," + p3 + "," + p2 + "," + p4
             else:
-                ordre = str(p1 + "," + p3 + "," + p4 + "," + p2)
+                ordre = p1 + "," + p3 + "," + p4 + "," + p2
         else: 
-            ordre = str(p1 + "," + p4)
+            ordre = p1 + "," + p4
             if p2 <= p3:
-                ordre =  str(p1 + "," + p4 + "," + p2 + "," + p3)
+                ordre = p1 + "," + p4 + "," + p2 + "," + p3
             else: 
-                ordre =  str(p1 + "," + p4 + "," + p3 + "," + p2)  
+                ordre = p1 + "," + p4 + "," + p3 + "," + p2
 
-    if p2 <= p1 and p3 and p4:
-        ordre = str(p2)
-        if p1 <= p3 and p4:
-            ordre = str(p2 + "," + p1)
+    if p2 <= p1 and p2 <= p3 and p2 <= p4:
+        ordre = p2
+        if p1 <= p3 and p1 <= p4:
+            ordre = p2 + "," + p1
             if p3 <= p4:
-                ordre = str(p2 + "," + p1 + "," + p3 + "," + p4)
+                ordre = p2 + "," + p1 + "," + p3 + "," + p4
             else:
-                ordre = str(p2 + "," + p1 + "," + p4 + "," + p3)
-        elif p3 <= p1 and p4:
-            ordre = str(p1 + "," + p3)
+                ordre = p2 + "," + p1 + "," + p4 + "," + p3
+        elif p3 <= p1 and p3 <= p4:
+            ordre = p1 + "," + p3
             if p1 <= p4:
-                ordre = str(p2 + "," + p3 + "," + p1 + "," + p4)
+                ordre = p2 + "," + p3 + "," + p1 + "," + p4
             else:
-                ordre = str(p2 + "," + p3 + "," + p4 + "," + p1)
+                ordre = p2 + "," + p3 + "," + p4 + "," + p1
         else: 
-            ordre = str(p2 + "," + p4)
+            ordre = p2 + "," + p4
             if p1 <= p3:
-                ordre =  str(p2 + "," + p4 + "," + p1 + "," + p3)
+                ordre = p2 + "," + p4 + "," + p1 + "," + p3
             else: 
-                ordre =  str(p2 + "," + p4 + "," + p3 + "," + p1)
+                ordre = p2 + "," + p4 + "," + p3 + "," + p1
 
-    if p3 <= p1 and p2 and p4:
-        ordre = str(p3)
-        if p1 <= p2 and p4:
-            ordre = str(p3 + "," + p1)
+    if p3 <= p1 and p3 <= p2 and p3 <= p4:
+        ordre = p3
+        if p1 <= p2 and p1 <= p4:
+            ordre = p3 + "," + p1
             if p2 <= p4:
-                ordre = str(p3 + "," + p1 + "," + p2 + "," + p4)
+                ordre = p3 + "," + p1 + "," + p2 + "," + p4
             else:
-                ordre = str(p3 + "," + p1 + "," + p4 + "," + p2) 
-        elif p2 <= p1 and p4:
-            ordre = str(p3 + "," + p2)
+                ordre = p3 + "," + p1 + "," + p4 + "," + p2
+        elif p2 <= p1 and p2 <= p4:
+            ordre = p3 + "," + p2
             if p1 <= p4:
-                ordre = str(p3 + "," + p2 + "," + p1 + "," + p4)
+                ordre = p3 + "," + p2 + "," + p1 + "," + p4
             else:
-                ordre = str(p3 + "," + p2 + "," + p4 + "," + p1)
+                ordre = p3 + "," + p2 + "," + p4 + "," + p1
         else: 
-            ordre = str(p3 + "," + p4)
+            ordre = p3 + "," + p4
             if p1 <= p2:
-                ordre =  str(p3 + "," + p4 + "," + p1 + "," + p2)
+                ordre = p3 + "," + p4 + "," + p1 + "," + p2
             else: 
-                ordre =  str(p3 + "," + p4 + "," + p2 + "," + p1)
+                ordre = p3 + "," + p4 + "," + p2 + "," + p1
 
-    if p4 <= p1 and p2 and p3:
-        ordre = str(p4)
-        if p1 <= p2 and p3:
-            ordre = str(p4 + "," + p1)
+    if p4 <= p1 and p4 <= p2 and p4 <= p3:
+        ordre = p4
+        if p1 <= p2 and p1 <= p3:
+            ordre = p4 + "," + p1
             if p2 <= p3:
-                ordre = str(p4 + "," + p1 + "," + p2 + "," + p3)
+                ordre = p4 + "," + p1 + "," + p2 + "," + p3
             else:
-                ordre = str(p4 + "," + p1 + "," + p3 + "," + p2)
-        elif p2 <= p1 and p3:
-            ordre = str(p4 + "," + p2)
+                ordre = p4 + "," + p1 + "," + p3 + "," + p2
+        elif p2 <= p1 and p2 <= p3:
+            ordre = p4 + "," + p2
             if p1 <= p3:
-                ordre = str(p4 + "," + p2 + "," + p1 + "," + p3)
+                ordre = p4 + "," + p2 + "," + p1 + "," + p3
             else:
-                ordre = str(p4 + "," + p2 + "," + p3 + "," + p1)
+                ordre = p4 + "," + p2 + "," + p3 + "," + p1
         else: 
-            ordre = str(p4 + "," + p3)
+            ordre = p4 + "," + p3
             if p1 <= p2:
-                ordre =  str(p4 + "," + p3 + "," + p1 + "," + p2)
+                ordre = p4 + "," + p3 + "," + p1 + "," + p2
             else: 
-                ordre =  str(p4 + "," + p3 + "," + p2 + "," + p1)
+                ordre = p4 + "," + p3 + "," + p2 + "," + p1
 
     return ordre
 
