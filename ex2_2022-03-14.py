c080613d3de41e52815a830ab4aecb822dd5c36a
David Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-03-14 19:46:05 -0400

    updated

--- ex2_2022-03-14.py
+++ ex2_2022-03-14.py
@@ -3,13 +3,13 @@
 #   L'état du ciel(Ensoleillé, nuageux ou éclaircies), les précipitations (aucune, pluie, neige ou verglas), la vitesse des vents en km/h et la 
 #   température en C.
 #Ensuite, affichez à l'écran un message d'alerte météorologique si les conditions suivantes sont respectées:
-#   A1 Il y a des précipitations et les vents sont supérieurs à 30 km/h
-#   A2 Il y a du verglas
-#   A3 Il fait soleil, mais les vents sont supérieurs à 70km/h ou les températures sont supérieures à 30C ou inférieures à -25C.
-#   A4 Il fait nuageux et les vents sont supérieurs à 50km/h.
-#   A5 Il fait nuageux et les vents sont supérieurs à 30km/h, mais la température est supérieure à 25C ou inférieure à -20C
-#   A6 Il neige et la température est inférieure à -25C.
-#   A7 Il fait ensoleillé, il n'y a pas de vent et la température est supérieure à 25C.
+#   Il y a des précipitations et les vents sont supérieurs à 30 km/h
+#   Il y a du verglas
+#   Il fait soleil, mais les vents sont supérieurs à 70km/h ou les températures sont supérieures à 30C ou inférieures à -25C.
+#   Il fait nuageux et les vents sont supérieurs à 50km/h.
+#   Il fait nuageux et les vents sont supérieurs à 30km/h, mais la température est supérieure à 25C ou inférieure à -20C
+#   Il neige et la température est inférieure à -25C.
+#   Il fait ensoleillé, il n'y a pas de vent et la température est supérieure à 25C.
 #Permettez à un utilisateur de rentrer les conditions météorologiques à l'aide d'un menu et assurez-vous qu'il soit impossible de rentrer des 
 #conditions météorologiques qui ne font pas de sens.
 
