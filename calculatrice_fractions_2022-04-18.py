8711f94a81c1faf7cc9237c85674f209b3224b70
daveonduty006 <95148064+daveonduty006@users.noreply.github.com>
2022-04-20 15:00:24 -0400

    initial add

--- calculatrice_fractions_2022-04-18.py
+++ calculatrice_fractions_2022-04-18.py
@@ -1,55 +1,71 @@
-def gcd(num, den): 
-    # Euclidean algorithm for finding the greatest common divisor
-    if num == 0:
-        return abs(den)
-    else:
-        return gcd(den % num, num)
-    
+import math
+
+#Implémenter une calculatrice de fraction en utilisant une classe fraction qui implémente les surcharges 
+#d'opérateurs suivants: add(+), sub(-), eq(==), mul(*), div(/), str(), pow(**)
+
+#Vous devez donc pouvoir faire des additions, soustractions, multiplications, divisions, et des puissances. 
+#Vous devez aussi pouvoir vérifier si 2 fractions sont égales (faites attention de vérifier les formes 
+#réduites, 1/2 == 2/4 devrait retourner True). Finalement vous devez surcharger str pour retourner votre 
+#fraction sous forme d'un string pour pouvoir l'imprimer.
+
+#Suite à l'implémentation, on devrait pouvoir écrire: fraction1 + fraction2 et avoir le résultat de 
+#l'addition. Le principe est le même pour tous les opérateurs.
+
+#Faites attention à pow(**), car on est censé faire (fraction1)^fraction2. Donc (4/1)**(1/2) = 2.
 
 class Fraction:
 
-    def __init__(self, f1_tuple, f2_tuple):
-        self.f1 = f"{f1_tuple[0]}/{f1_tuple[1]}"
-        self.f2 = f"{f2_tuple[0]}/{f2_tuple[1]}"
+    def __init__(self, num, den):
+        self.num = num
+        self.den = den
+        gcd = self.get_gcd()
+        self.num = self.num // gcd
+        self.den = self.den // gcd
 
-def fraction_input():
-    exit = False
-    while not exit: 
-        f1_num = input("Entrez le numérateur de votre première fraction: ")
-        if f1_num.lstrip("-").isdecimal():
-            f1_den = input("Entrez le dénominateur de votre première fraction: ")
-            if f1_den.lstrip("-").isdecimal():
-                f2_num = input("Entrez le numérateur de votre deuxième fraction: ")
-                if f2_num.lstrip("-").isdecimal():
-                    f2_den = input("Entrez le dénominateur de votre deuxième fraction: ")
-                    if f2_den.lstrip("-").isdecimal(): 
-                        exit = True
-    f1_tuple = (f1_num, f1_den) 
-    f2_tuple = (f2_num, f2_den)
-    return f1_tuple, f2_tuple
+    def get_gcd(self):
+        m, n = self.num, self.den
+        while m % n != 0:
+            oldm = m
+            oldn = n
+            m = oldn
+            n = oldm % oldn
+        return n
 
-def execution():
-    f1_tuple, f2_tuple = fraction_input()
-    f1, f2 = Fraction(f1_tuple, f2_tuple)
-    print(f1)
-    exit = False
-    while not exit:
-        user_sel = 0
-        while not 1 <= user_sel <= 6:
-            user_sel = int(input(f"Menu des options:\n"
-                                 f"1. Additionner les fractions\n"
-                                 f"2. Soustraire les fractions\n"
-                                 f"3. Multiplier les fractions\n"
-                                 f"4. Diviser les fractions\n"
-                                 f"5. Modifier les fractions\n"
-                                 f"6. Terminer\n"
-                                 f"Choisissez l'une des options: "))
+    def __add__(self, f2):
+        self.num = (self.num * f2.den) + (self.den * f2.num)
+        self.den = self.den * f2.den
+        gcd = self.get_gcd()
+        return Fraction(self.num//gcd, self.den//gcd)
+
+    def __sub__(self, f2):
+        self.num = (self.num * f2.den) - (self.den * f2.num)
+        self.den = self.den * f2.den
+        gcd = self.get_gcd()
+        return Fraction(self.num//gcd, self.den//gcd)
+
+    def __mul__(self, f2):
+        self.num = self.num * f2.num 
+        self.den = self.den * f2.den
+        gcd = self.get_gcd()
+        return Fraction(self.num//gcd, self.den//gcd)
+
+    def __truediv__(self, f2):
+        self.num = self.num * f2.den
+        self.den = self.den * f2.num
+        gcd = self.get_gcd()
+        return Fraction(self.num//gcd, self.den//gcd)
+
+    def __pow__(self, f2):
+        return f"{math.pow(self.num, f2.num/f2.den)/math.pow(self.den, f2.num/f2.den):.2f}"
+
+    def __eq__(self, f2):
+        return self.num * f2.den == self.den * f2.num
+
+    def __str__(self):
+        return f"{self.num}/{self.den}"
 
 
-
-
-
-execution()
-
-        
-
+f1 = Fraction(4,1)
+f2 = Fraction(1,2)
+f3 = f1 * f2
+print(f3)
